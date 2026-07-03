a035598da896cc9e907670a55bd33a435f6eb484
m5p7nmmk9j-tech <m5p7nmmk9j@privaterelay.appleid.com>
2026-07-03 15:53:20 +0800

    web: add rcv menu

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -3,22 +3,20 @@
     <el-aside width="240px" class="aside">
       <div class="brand">FUTUREM Sourcing</div>
       <el-menu router default-active="/" class="menu">
-        <el-menu-item index="/">首页 Dashboard</el-menu-item>
-        <el-menu-item index="/products">商品管理</el-menu-item>
-        <el-menu-item index="/customers">客户管理</el-menu-item>
-        <el-menu-item index="/suppliers">供应商管理</el-menu-item>
-        <el-menu-item index="/markets">市场管理</el-menu-item>
-        <el-menu-item index="/rfqs">RFQ 客户询价</el-menu-item>
-        <el-menu-item index="/customer-orders">CO 客户订单</el-menu-item>
-        <el-menu-item index="/purchase-orders">PO 采购订单</el-menu-item>
-        <el-menu-item index="/so-orders">SO 汇总单</el-menu-item>
+        <el-menu-item index="/">Dashboard</el-menu-item>
+        <el-menu-item index="/products">Products</el-menu-item>
+        <el-menu-item index="/customers">Customers</el-menu-item>
+        <el-menu-item index="/suppliers">Suppliers</el-menu-item>
+        <el-menu-item index="/markets">Markets</el-menu-item>
+        <el-menu-item index="/rfqs">RFQ</el-menu-item>
+        <el-menu-item index="/customer-orders">CO</el-menu-item>
+        <el-menu-item index="/purchase-orders">PO</el-menu-item>
+        <el-menu-item index="/so-orders">SO</el-menu-item>
+        <el-menu-item index="/receiving-orders">Receiving</el-menu-item>
       </el-menu>
     </el-aside>
     <el-container>
-      <el-header class="header">
-        <div class="search">全局搜索：客户 / 供应商 / SKU / RFQ / CO / PO / SO</div>
-        <div>V0.1 Alpha</div>
-      </el-header>
+      <el-header class="header"><div class="search">Global Search</div><div>V0.1 Alpha</div></el-header>
       <el-main><router-view /></el-main>
     </el-container>
   </el-container>
