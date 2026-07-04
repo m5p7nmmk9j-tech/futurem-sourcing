32ac42ab7833559464588eba27fe14d353849825
m5p7nmmk9j-tech <m5p7nmmk9j@privaterelay.appleid.com>
2026-07-04 19:45:29 +0800

    api: add dashboard daily and monthly metrics

--- api/Futurem.Sourcing.Api/Controllers/BusinessDashboardController.cs
+++ api/Futurem.Sourcing.Api/Controllers/BusinessDashboardController.cs
@@ -14,13 +14,21 @@ public class BusinessDashboardController : ControllerBase
     [HttpGet("summary")]
     public async Task<ActionResult<object>> Summary()
     {
+        var today = DateTime.Today;
+        var monthStart = new DateTime(today.Year, today.Month, 1);
         var receivables = await _db.FinanceRecords.Where(x => x.RecordType == "receivable").ToListAsync();
         var payables = await _db.FinanceRecords.Where(x => x.RecordType == "payable").ToListAsync();
+        var expenses = await _db.FinanceRecords.Where(x => x.RecordType == "expense").ToListAsync();
+        var incomes = await _db.FinanceRecords.Where(x => x.RecordType == "income").ToListAsync();
+        var payments = await _db.Payments.ToListAsync();
         var containers = await _db.ContainerLoads.ToListAsync();
         var shipments = await _db.Shipments.ToListAsync();
         var po = await _db.PurchaseOrders.ToListAsync();
         var so = await _db.SummaryOrders.ToListAsync();
         var qc = await _db.QcOrders.ToListAsync();
+
+        decimal IncomeAmount(DateTime from, DateTime to) => receivables.Where(x => (x.RecordDate ?? x.CreatedAt).Date >= from && (x.RecordDate ?? x.CreatedAt).Date <= to).Sum(x => x.Amount) + incomes.Where(x => (x.RecordDate ?? x.CreatedAt).Date >= from && (x.RecordDate ?? x.CreatedAt).Date <= to).Sum(x => x.Amount);
+        decimal CostAmount(DateTime from, DateTime to) => payables.Where(x => (x.RecordDate ?? x.CreatedAt).Date >= from && (x.RecordDate ?? x.CreatedAt).Date <= to).Sum(x => x.Amount) + expenses.Where(x => (x.RecordDate ?? x.CreatedAt).Date >= from && (x.RecordDate ?? x.CreatedAt).Date <= to).Sum(x => x.Amount);
 
         return new
         {
@@ -40,6 +48,28 @@ public class BusinessDashboardController : ControllerBase
                 qcOrders = qc.Count,
                 containerLoads = containers.Count,
                 shipments = shipments.Count
+            },
+            today = new
+            {
+                rfqs = await _db.Rfqs.CountAsync(x => x.CreatedAt.Date == today),
+                customerOrders = await _db.CustomerOrders.CountAsync(x => x.CreatedAt.Date == today),
+                purchaseOrders = po.Count(x => x.CreatedAt.Date == today),
+                summaryOrders = so.Count(x => x.CreatedAt.Date == today),
+                receivingOrders = await _db.ReceivingOrders.CountAsync(x => x.CreatedAt.Date == today),
+                qcOrders = qc.Count(x => x.CreatedAt.Date == today),
+                containerLoads = containers.Count(x => x.CreatedAt.Date == today),
+                shipments = shipments.Count(x => x.CreatedAt.Date == today),
+                received = payments.Where(x => x.Direction == "receive" && x.PaymentDate.Date == today).Sum(x => x.Amount),
+                paid = payments.Where(x => x.Direction == "pay" && x.PaymentDate.Date == today).Sum(x => x.Amount),
+                profit = IncomeAmount(today, today) - CostAmount(today, today)
+            },
+            month = new
+            {
+                sales = so.Where(x => x.OrderDate.HasValue && x.OrderDate.Value.Date >= monthStart).Sum(x => x.ReceivableAmount),
+                purchase = payables.Where(x => (x.RecordDate ?? x.CreatedAt).Date >= monthStart && x.TargetType == "PO").Sum(x => x.Amount),
+                received = payments.Where(x => x.Direction == "receive" && x.PaymentDate.Date >= monthStart).Sum(x => x.Amount),
+                paid = payments.Where(x => x.Direction == "pay" && x.PaymentDate.Date >= monthStart).Sum(x => x.Amount),
+                profit = IncomeAmount(monthStart, today) - CostAmount(monthStart, today)
             },
             finance = new
             {
@@ -64,26 +94,10 @@ public class BusinessDashboardController : ControllerBase
     [HttpGet("todo")]
     public async Task<ActionResult<object>> Todo()
     {
-        var pendingReceivables = await _db.FinanceRecords
-            .Where(x => x.RecordType == "receivable" && x.Status != "done")
-            .OrderBy(x => x.RecordDate ?? x.CreatedAt)
-            .Take(20)
-            .ToListAsync();
-        var pendingPayables = await _db.FinanceRecords
-            .Where(x => x.RecordType == "payable" && x.Status != "done")
-            .OrderBy(x => x.RecordDate ?? x.CreatedAt)
-            .Take(20)
-            .ToListAsync();
-        var pendingContainers = await _db.ContainerLoads
-            .Where(x => x.Status != "shipment_created")
-            .OrderByDescending(x => x.Id)
-            .Take(20)
-            .ToListAsync();
-        var pendingShipments = await _db.Shipments
-            .Where(x => x.Status != "arrived" && x.Status != "done")
-            .OrderBy(x => x.Eta ?? x.Etd ?? x.CreatedAt)
-            .Take(20)
-            .ToListAsync();
+        var pendingReceivables = await _db.FinanceRecords.Where(x => x.RecordType == "receivable" && x.Status != "done").OrderBy(x => x.RecordDate ?? x.CreatedAt).Take(20).ToListAsync();
+        var pendingPayables = await _db.FinanceRecords.Where(x => x.RecordType == "payable" && x.Status != "done").OrderBy(x => x.RecordDate ?? x.CreatedAt).Take(20).ToListAsync();
+        var pendingContainers = await _db.ContainerLoads.Where(x => x.Status != "shipment_created").OrderByDescending(x => x.Id).Take(20).ToListAsync();
+        var pendingShipments = await _db.Shipments.Where(x => x.Status != "arrived" && x.Status != "done").OrderBy(x => x.Eta ?? x.Etd ?? x.CreatedAt).Take(20).ToListAsync();
 
         return new
         {
